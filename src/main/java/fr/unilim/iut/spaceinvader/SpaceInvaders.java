d075ef748928d712df4c76c311350fc090fa097d
fares <fares.cherif@etu.unilim.fr>
2019-04-08 16:41:59 +0200

    classe vaisseau ajouté

--- src/main/java/fr/unilim/iut/spaceinvader/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvader/SpaceInvaders.java
@@ -3,6 +3,8 @@ public class SpaceInvaders {
 
     int longueur;
     int hauteur;
+	Vaisseau vaisseau;
+
 
     public SpaceInvaders(int longueur, int hauteur) {
 	   this.longueur = longueur;
@@ -12,13 +14,19 @@ public class SpaceInvaders {
     @Override
 	public String toString() {
 		StringBuilder espaceDeJeu = new StringBuilder();
-		for (int i = 0; i < hauteur; i++) {
-			for (int j = 0; j < longueur; j++) {
-				espaceDeJeu.append('.');
+		for (int y = 0; y < hauteur; y++) {
+			for (int x = 0; x < longueur; x++) {
+				if (vaisseau!=null && vaisseau.occupeLaPosition(x, y))
+					espaceDeJeu.append('V');
+				else
+					espaceDeJeu.append('.');
 			}
 			espaceDeJeu.append('\n');
 		}
 		return espaceDeJeu.toString();
 	}
-
+    
+    public void positionnerUnNouveauVaisseau(int x, int y) {
+        this.vaisseau = new Vaisseau(x, y);
+	}
 }
